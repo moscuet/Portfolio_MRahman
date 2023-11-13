521e4f51dea534a48ebd4cd1a09452497f932282
Mostafizur Rahman <51766137+moscuet@users.noreply.github.com>
2023-11-13 23:43:21 +0200

    installl axios (#7)

--- lib/getProject.tsx
+++ lib/getProject.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import axios from 'axios';
 
 export default async function getProject(id: string) {
-  const res = await fetch(`http://localhost:3000/api/projects/${id}`, {
-    method: 'GET',
-    headers: {
-      'Cache-Control': 'no-cache'
-    }
-  });
+  try {
+    const response = await axios.get(`http://localhost:3000/api/projects/${id}`, {
+      headers: {
+        'Cache-Control': 'no-cache'
+      }
+    });
 
-  if (!res.ok) throw new Error('Failed to fetch data');
-  return res.json();
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch data');
+  }
 }
