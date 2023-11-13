b82c6add8145018a060c528be2b757af57dba5f1
moscuet <mos.cuet@gmail.com>
2023-11-13 23:31:09 +0200

    build to deploy

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,11 @@ import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Button } from '@mui/material';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function NavBar() {
     const [isNavOpen, setIsNavOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(typeof window !== "undefined" && window.innerWidth < 1024); // Or your mobile breakpoint
+    const [isMobile, setIsMobile] = useState(false);
 
     const toggleNav = () => {
         setIsNavOpen(!isNavOpen);
@@ -17,28 +18,28 @@ export default function NavBar() {
     };
 
     useEffect(() => {
+        updateMedia();
         window.addEventListener('resize', updateMedia);
         return () => window.removeEventListener('resize', updateMedia);
-    });
-
-
-  
+    }, []);
     return (
         <>
             <div style={{ height: '100px' }} className="fixed top-0 left-0 right-0 z-10 flex flex-row justify-between items-center w-full pr-4 bg-gray-100 text-gray-800">
-            <div className="flex items-center" style={{ height: '100%' }}>
-                <Link href="/" passHref style={{ height: '100%' }}>
-                    <img 
-                        src={'/assets/brand_logo.png'} 
-                        alt="Logo" 
-                        style={{ height: '100%', cursor: 'pointer' }}
-                    />
-                </Link>
-            </div>
+                <div className="flex items-center" style={{ height: '100%' }}>
+                    <Link href="/" passHref>
+                        <Image
+                            src={'/assets/brand_logo.png'}
+                            alt="Logo"
+                            width={100} 
+                            height={50}
+                            style={{ cursor: 'pointer' }}
+                        />
+                    </Link>
+                </div>
 
                 <div className="flex items-center">
                     <nav className="hidden lg:flex mr-4">
-                    <a href="/" className="nav-link">PORTFOLIO</a>
+                        <a href="/" className="nav-link">PORTFOLIO</a>
                         <a href="/about" className="nav-link">ABOUT</a>
                         <a href="/contact" className="nav-link">CONTACT</a>
                     </nav>
@@ -56,7 +57,7 @@ export default function NavBar() {
                     <button onClick={toggleNav} className="text-indigo-500 self-end">
                         <FaTimes size={24} />
                     </button>
-                    
+
                     <nav className="mt-4 flex flex-col">
                         <a href="#about-section" style={{ color: 'rgb(31, 41, 55)' }} className="nav-link mb-2 hover:text-indigo-600">ABOUT</a>
                         <a href="#projects-section" style={{ color: 'rgb(31, 41, 55)' }} className="nav-link mb-2 hover:text-indigo-600">PROJECTS</a>
