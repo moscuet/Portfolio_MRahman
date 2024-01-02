e14ecfe419c15b040b71352212545a74efe72347
Mostafizur Rahman <51766137+moscuet@users.noreply.github.com>
2024-01-02 12:59:46 +0200

    Develop (#26)
    * modifyng layout of project tempalte
    
    * modify project explore section

--- src/components/projects/ExploreProjectSection.tsx
+++ src/components/projects/ExploreProjectSection.tsx
@@ -19,10 +19,12 @@ const ExploreProjectSection = ({ github, url }: { github: string, url: string })
   };
 
   return (
-    <Box className="py-5  mb-5 bg-gray-100 rounded-lg shadow-md ">
+
+    <Box border={{border:'2px solid red'}} className="py-5 px-1 sm:px-2 lg:px-4 mb-5 bg-gray-100 rounded-lg shadow-md ">
+
 
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={12} md={4} className="flex items-center justify-start">
+      <Grid item xs={12} sm={12} md={4} className="flex items-center justify-center lg:justify-start">
           <h2 className="text-xl md:text-2xl font-bold text-gray-800">Explore the Project</h2>
         </Grid>
 
