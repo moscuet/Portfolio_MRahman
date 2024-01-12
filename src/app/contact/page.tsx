a94dec3925c09fc6c6b00b6f3d82f6db6b587dc4
Mostafizur Rahman <51766137+moscuet@users.noreply.github.com>
2024-01-12 10:10:31 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,7 +15,7 @@ const Contact: FC = () => {
     return (
         <div className='w-screen -ml-12  flex flex-col items-center justify-center pl-14 sm:pl-0 pt-[100px]'>
             <div className='flex justify-center w-full max-w-xl'>
-                <span className='w-full text-center text-gray-800 text-sm sm:text-lg mt-10 p-1'>Have an idea in mind? Let's bring it to life together!</span>
+                <span className='w-full text-center text-gray-800 text-sm sm:text-lg mt-10 p-1'>Have an idea in mind? Let&apos;s bring it to life together!</span>
             </div>
             <div  className='w-full max-w-xl bg-gray-50 rounded-lg mb-10 mt-5 sm:p-10 bg-gradient-to-b from-indigo-300 to-indigo-100'>
                 <ContactForm />
