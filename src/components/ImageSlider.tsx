1957ce21884a3cb8a89b182df98e9ba1f0c42f11
moscuet <mos.cuet@gmail.com>
2024-02-15 16:55:11 +0200

    updated features section

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,69 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import { Box, IconButton } from '@mui/material';
-import Image from 'next/image';
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import React, { useState, useEffect } from "react";
+import { Box, IconButton } from "@mui/material";
+import Image from "next/image";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 
 interface ImageSliderProps {
-    images: string[];
-    url: string,
-    projectName: string
+  images: string[];
+  url: string;
+  projectName: string;
 }
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
-    const [current, setCurrent] = useState(0);
-    const length = images.length;
+  const [current, setCurrent] = useState(0);
+  const length = images.length;
 
-  
-    useEffect(() => {
-        const nextSlide = () => {
-            setCurrent(current === length - 1 ? 0 : current + 1);
-        };    
-        const timer = setInterval(nextSlide, 3000);
-        return () => clearInterval(timer);
-    }, [current,length]);
+  useEffect(() => {
+    const nextSlide = () => {
+      setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+    const timer = setInterval(nextSlide, 3000);
+    return () => clearInterval(timer);
+  }, [current, length]);
 
-    if (!Array.isArray(images) || images.length === 0) {
-        return null;
+  if (!Array.isArray(images) || images.length === 0) {
+    return null;
+  }
+
+  return (
+    <>
+      <style>
+        {`
+  .dynamic-height {
+    height: calc((100vw - 96px) * 9 / 16);
+  }
+  @media (min-width: 921px) {
+    .dynamic-height {
+      height: 100%; /* Adjust this as needed */
     }
+  }
+`}
+      </style>
+      <div className="w-full dynamic-height">
+        <Box
+          style={{ height: "100%" }}
+          sx={{ position: "relative", mx: "auto", overflow: "hidden" }}
+        >
+          {images.map((image, index) => {
+            let style;
+            if (index === current) {
+              style = {
+                transform: "translateX(0%)",
+                transition: "transform 0.5s ease-out",
+              };
+            } else if (index === (current + 1) % length) {
+              style = {
+                transform: "translateX(100%)",
+                transition: "transform 0.5s ease-out",
+              };
+            } else {
+              style = { transform: "translateX(100%)" };
+            }
 
-    return (
-        <Box style={{ height: '100%' }} sx={{ position: 'relative', mx: 'auto', overflow: 'hidden' }}>
-            {images.map((image, index) => {
-                let style;
-                if (index === current) {
-                    style = { transform: 'translateX(0%)', transition: 'transform 0.5s ease-out' };
-                } else if (index === (current + 1) % length) {
-                    style = { transform: 'translateX(100%)', transition: 'transform 0.5s ease-out' };
-                } else {
-                    style = { transform: 'translateX(100%)' };
-                }
+            return (
+              <div
+                key={index}
+                style={{
+                  ...style,
+                  height: "100%",
+                  width: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                }}
+              >
+                <Image
+                  src={`/assets/images/${image}`}
+                  alt={`Slide ${index}`}
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
+            );
+          })}
 
-                return (
-                    <div
-                        key={index}
-                        style={{
-                            ...style,
-                            height: '100%',
-                            width: '100%',
-                            position: 'absolute',
-                            top: 0,
-                            left: 0,
-                        }}
-                    >
-                        <Image src={`/assets/images/${image}`} alt={`Slide ${index}`} layout='fill' objectFit="contain" />
-                    </div>
-                );
-            })}
-
-            <Box sx={{ position: 'absolute', bottom: '-2px', left: '50%', transform: 'translateX(-50%)', display: 'flex' }}>
-                {images.map((_, idx) => (
-                    <IconButton key={idx} onClick={() => setCurrent(idx)} sx={{ padding: 0, color: current === idx ? 'primary.main' : 'grey.500', opacity: current === idx ? 1 : 0.3 }}>
-                        <FiberManualRecordIcon fontSize="small" />
-                    </IconButton>
-                ))}
-            </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "-2px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              display: "flex",
+            }}
+          >
+            {images.map((_, idx) => (
+              <IconButton
+                key={idx}
+                onClick={() => setCurrent(idx)}
+                sx={{
+                  padding: 0,
+                  color: current === idx ? "primary.main" : "grey.500",
+                  opacity: current === idx ? 1 : 0.3,
+                }}
+              >
+                <FiberManualRecordIcon fontSize="small" />
+              </IconButton>
+            ))}
+          </Box>
         </Box>
-    );
+      </div>
+    </>
+  );
 };
 
 export default ImageSlider;
--- src/components/projects/ProjectTemplate.tsx
+++ src/components/projects/ProjectTemplate.tsx
@@ -1,56 +1,74 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import { Grid } from '@mui/material';
-import MyRoleAndContributions from '@/components/projects/MyRoleAndContribution';
-import ExploreProjectSection from '@/components/projects/ExploreProjectSection';
-import ProjectStorySection from '@/components/projects/StorySection';
-import TechStackSection from '@/components/projects/TechStackSection';
-import ProjecImageBanner from '@/components/projects/ProjectImageBanner';
-import ProjectFeatures from './ProjectFeatures';
+"use client";
+import React, { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
+import MyRoleAndContributions from "@/components/projects/MyRoleAndContribution";
+import ExploreProjectSection from "@/components/projects/ExploreProjectSection";
+import ProjectStorySection from "@/components/projects/StorySection";
+import TechStackSection from "@/components/projects/TechStackSection";
+import ProjecImageBanner from "@/components/projects/ProjectImageBanner";
+import ProjectFeatures from "./ProjectFeatures";
 
+const ProjectTemplate: React.FC<ProjectType> = ({
+  title,
+  images,
+  github,
+  url,
+  techStack,
+  story,
+  roles,
+  features,
+}) => {
+  return (
+    <Grid container className="flex flex-col items-center">
+      <Grid
+        item
+        sx={{
+          width: "100%",
+          "@media (min-width: 780px)": {
+            height: "calc(100vh - 180px)",
+          },
+        }}
+      >
+        <ProjecImageBanner
+          title={title}
+          images={images}
+          url={url}
+          github={github}
+        />
+      </Grid>
 
+      <Grid
+        container
+        item
+        className="flex  flex-col w-full justify-center max-w-5xl"
+      >
+        <Grid item xs={12} mb={6} py={5} mt={5}>
+          <ProjectStorySection story={story} />
+        </Grid>
 
-const ProjectTemplate: React.FC<ProjectType> = ({ title, images, github, url, techStack, story, roles, features }) => {
+        <Grid item xs={12} mb={6} py={5}>
+          <TechStackSection techStack={techStack} />
+        </Grid>
+        {features && (
+          <Grid item xs={12} mb={6} py={5}>
+            <ProjectFeatures features={features} />
+          </Grid>
+        )}
 
+        <Grid item xs={12} mb={6} py={5}>
+          <MyRoleAndContributions roles={roles} />
+        </Grid>
 
-    return (
-        <Grid container className="flex flex-col items-center">
-            <Grid item style={{ width: '100%', height: 'calc(100vh - 180px)' }}>
-                <ProjecImageBanner
-                    title={title}
-                    images={images}
-                    url={url}
-                    github={github}
-                />
-            </Grid>
-
-            <Grid container item className="flex  flex-col w-full justify-center max-w-5xl" >
-
-                <Grid item xs={12} mb={6} py={5} mt={5}>
-                    <ProjectStorySection story={story} />
-                </Grid>
-
-                <Grid item xs={12} mb={6} py={5}  >
-                    <TechStackSection techStack={techStack} />
-                </Grid>
-               {features && <Grid item xs={12} mb={6} py={5}  >
-                    <ProjectFeatures features={features}/>
-                </Grid>}
-
-                <Grid item xs={12} mb={6} py={5}  >
-                    <MyRoleAndContributions roles={roles} />
-                </Grid>
-
-                {/* <Grid item xs={12} mb={6} py={5}  >
+        {/* <Grid item xs={12} mb={6} py={5}  >
                     <DevelopmentTimeline developmentSteps={developmentSteps} />
                 </Grid> */}
 
-                <Grid item xs={12} mb={6}  >
-                    <ExploreProjectSection github={github} url={url} />
-                </Grid>
-            </Grid>
+        <Grid item xs={12} mb={6}>
+          <ExploreProjectSection github={github} url={url} />
         </Grid>
-    );
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ProjectTemplate;
